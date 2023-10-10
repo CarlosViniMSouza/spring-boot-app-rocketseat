91aa9f975a5c9b1b6a388cd14e14d6f924c891ab
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2023-10-10 09:23:14 -0300

    stopped: Lombok

---
+++ autogit.sh
@@ -0,0 +1,9 @@
+git init
+git add .
+git commit -m "stopped: Lombok"
+git branch -M main
+git remote add origin https://github.com/CarlosViniMSouza/spring-boot-app-rocketseat.git
+git push -u origin main
+git status
+
+# paused in 3:23 - lesson 02
