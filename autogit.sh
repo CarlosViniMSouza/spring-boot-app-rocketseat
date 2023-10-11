53e1a87464cd9f753b30da4e61a09b369eaac069
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2023-10-11 11:06:22 -0300

    stopped: Repository

--- autogit.sh
+++ autogit.sh
@@ -1,6 +1,6 @@
 git add .
-git commit -m "stopped: User Table"
+git commit -m "stopped: Repository"
 git push -u origin main
 git status
 
-# paused in 16:18 - lesson 02
+# paused in 23:45 - lesson 02
