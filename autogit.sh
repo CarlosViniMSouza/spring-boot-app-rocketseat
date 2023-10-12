220de70d4b4996a9b1197fcfa71a3da41c8ca1a6
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2023-10-11 23:05:43 -0300

    stopped: Password Decode

--- autogit.sh
+++ autogit.sh
@@ -1,6 +1,6 @@
 git add .
-git commit -m "stopped: Continuing Filtering"
+git commit -m "stopped: Password Decode"
 git push -u origin main
 git status
 
-# completed in 26:37 - lesson 03
+# completed in 38:58 - lesson 03
--- src/main/java/br/com/qrmenu/apiserver/filter/FilterTaskAuth.java
+++ src/main/java/br/com/qrmenu/apiserver/filter/FilterTaskAuth.java
@@ -1,21 +1,43 @@
 package br.com.qrmenu.apiserver.filter;
 
 import java.io.IOException;
+import java.util.Base64;
 
 import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 
-import jakarta.servlet.Filter;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.ServletRequest;
-import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @Component
-public class FilterTaskAuth implements Filter {
+public class FilterTaskAuth extends OncePerRequestFilter {
 
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-            throws IOException, ServletException {
-        chain.doFilter(request, response);
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        // Pega Usuario e Senha ->
+        var auth = request.getHeader("Authorization");
+        var authEncoded = auth.substring("Basic".length()).trim();
+
+        byte[] authDecode = Base64.getDecoder().decode(authEncoded);
+        var authString = new String(authDecode);
+
+        String[] credentials = authString.split(":");
+        String username = credentials[0];
+        String password = credentials[1];
+
+        System.out.println("Authorization: ");
+        System.out.println(username);
+        System.out.println(password);
+        // Valida Usuario ->
+
+        // Valida Senha ->
+
+        // Segue viagem
+        filterChain.doFilter(request, response);
     }
+
 }
